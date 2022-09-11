f010cd59085f8c242fc287bd4a0fd75e3d4e0757
Alisa Raevskaya <alisa@madebyforward.com>
2022-09-11 23:18:57 +0300

    Add styles

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,6 +1,6 @@
 
 import './bootstrap';
-import '../css/app.css';
+import '../sass/app.scss';
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from './App';
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/main.jsx'],
+            input: ['resources/sass/app.scss', 'resources/js/main.jsx'],
             refresh: true,
         }),
         react()
